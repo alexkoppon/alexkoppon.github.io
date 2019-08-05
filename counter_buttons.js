8c20c6f8e96ffc1a8d031f5d304c45e0c37f54fb
Alex <Alex>
2019-08-05 20:58:08 +1000

    testUpdateTally4

--- counter_buttons.js
+++ counter_buttons.js
@@ -5,8 +5,7 @@
 class HonbaButton extends React.Component {
     constructor(props) {
     super(props);
-    this.state = {on: false};
-    this.tally = 0;
+    this.state = {on: false, tally: 0};
 
     // This binding is necessary to make `this` work in the callback
     this.IncrementCounter = this.IncrementCounter.bind(this);
@@ -14,21 +13,21 @@ class HonbaButton extends React.Component {
     }
 
     IncrementCounter() {
-        let newTally = this.tally + 1;
+        let newTally = this.state.tally + 1;
         console.log(newTally);
         this.setState({
             tally: {newTally}
         });
-        console.log(this.tally);
+        console.log(this.state.tally);
     }
 
     DecrementCounter() {
-        let newTally = (this.tally > 0 ? this.tally- 1 : 0);
+        let newTally = ((this.state.tally > 0) ? this.state.tally - 1 : 0);
         console.log(newTally);
         this.setState({
             tally: {newTally}
         });
-        console.log(this.tally);
+        console.log(this.state.tally);
     }
 
     render() {
@@ -36,7 +35,7 @@ class HonbaButton extends React.Component {
             <div>
                 <label>Honba</label>            
                 <button className="selectable_features_buttons" onClick={this.DecrementCounter} id="honba_-">-</button>
-                <label id="honba_display">{this.tally}</label>
+                <label id="honba_display">{this.state.tally}</label>
                 <button className="selectable_features_buttons" onClick={this.IncrementCounter} id="honba_+">+</button>
             </div>
         )           
