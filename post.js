5bcf9fd00991772c1650a68435446aa4c4028e99
Alex <Alex>
2019-08-09 14:07:29 +1000

    -

--- post.js
+++ post.js
@@ -10,7 +10,6 @@ calculateHand.addEventListener("click", () => {
     let testRichii = document.getElementById('richii');
     console.log(testRichii);
     console.log(RichiiButton.toString);
-    console.log(RichiiButton.arguments);
     console.log(RichiiButton.length);
     console.log(RichiiButton.name);
     console.log(RichiiButton.state);
