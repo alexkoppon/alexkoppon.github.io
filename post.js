d373c1627d416f7c273ac1427b8be26593373b92
Alex <Alex>
2019-08-09 14:04:28 +1000

    test

--- post.js
+++ post.js
@@ -9,6 +9,13 @@ calculateHand.addEventListener("click", () => {
     // get the status of the relevant buttons etc 
     let testRichii = document.getElementById('richii');
     console.log(testRichii);
+    console.log(RichiiButton.toString);
+    console.log(RichiiButton.arguments);
+    console.log(RichiiButton.length);
+    console.log(RichiiButton.name);
+    console.log(RichiiButton.state);
+    console.log(RichiiButton.prototype);
+
 
 });
 
