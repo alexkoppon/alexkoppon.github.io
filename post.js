b1db6a13bac7083037e4304aea3e28fa94c4d65d
Alex <Alex>
2019-08-09 13:45:05 +1000

    minor fix

---
+++ post.js
@@ -0,0 +1,15 @@
+// import relevant modules
+import React from "react";
+import ReactDOM from "react-dom";
+
+
+const calculateHand = document.getElementById("calculate");
+
+calculateHand.addEventListener("click", () => {
+
+    // get the status of the relevant buttons etc 
+    let testRichii = document.getElementById('richii');
+    console.log(testRichii);
+
+});
+
